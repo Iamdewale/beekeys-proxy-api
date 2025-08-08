fee30a6e1a70d05720ebc222eb3c0ab39ac9f181
Iamdewale <dewalefams@gmail.com>
2025-08-08 19:13:51 +0100

    contributors page created

--- api/home.js
+++ api/home.js
@@ -1,3 +1,11 @@
-module.exports = (req, res) => {
-  res.status(200).json({ message: "Test endpoint working!" });
+const fetch = require('node-fetch');
+
+module.exports = async (req, res) => {
+  try {
+    const response = await fetch('https://api.publicapis.org/entries');
+    const data = await response.json();
+    res.status(200).json({ message: "Test endpoint working!", data: data.entries.slice(0, 3) });
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching data", error: error.message });
+  }
 };
